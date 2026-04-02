8f96578720a762aafce7d052e96c1099d3f893a3
PedroMagalhães <pedrodejesusmagalhaes@gmail.com>
2026-04-02 16:00:54 -0300

    Create metodos_de_listas.py

---
+++ Nivel_iniciante/listas/metodos_de_listas.py
@@ -0,0 +1,8 @@
+//
+append(elemento): adiciona um elemento ao final da lista.
+insert(indice, elemento): insere um elemento em uma posição específica da lista.
+remove(elemento): remove a primeira ocorrência de um elemento na lista.
+pop(indice): remove e retorna o elemento em uma posição específica da lista.
+sort(): ordena os elementos da lista em ordem ascendente.
+reverse(): inverte a ordem dos elementos na lista.
+//
