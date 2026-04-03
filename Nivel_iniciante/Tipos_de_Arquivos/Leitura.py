bb041f05798f28f298e7ec7c07709aeb9050555a
PedroMagalhães <pedrodejesusmagalhaes@gmail.com>
2026-04-03 02:02:50 -0300

    Update Leitura.py

--- Nivel_iniciante/Tipos_de_Arquivos/Leitura.py
+++ Nivel_iniciante/Tipos_de_Arquivos/Leitura.py
@@ -2,4 +2,5 @@
 arquivo = open("dados.txt", "r")
 conteudo = arquivo.read()
 print(conteudo)
+#A função clove serve para travar o arquivo e NÃO permitir que o usuário modifique o texto dentro do arquivo
 arquivo.close()
